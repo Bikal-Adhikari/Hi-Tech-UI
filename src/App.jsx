58e1d253a444548fd0f566f9e3263e382c16e295
Bikal <bikaladhikari5@gmail.com>
2024-07-20 21:13:36 +1000

    Error page route is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/landing page/Landing";
+import ErrorPage from "./pages/error page/ErrorPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="*" element={<ErrorPage/>} />
     </Routes>
   );
 }
