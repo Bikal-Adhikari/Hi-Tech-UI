a6447ae7a18b58fe05406cfeadd77cb8b7e68c34
Bikal <bikaladhikari5@gmail.com>
2024-07-26 18:17:29 +1000

    Forget password page created

--- src/pages/user/ForgetPassword.jsx
+++ src/pages/user/ForgetPassword.jsx
@@ -1,13 +1,75 @@
-import { Footer } from "../../components/layout/Footer/Footer";
-import { Header } from "../../components/layout/Header/Header";
+import { useState } from "react";
+import { RequestOTP } from "../../components/password-reset/RequestOTP";
+import { ResetPass } from "../../components/password-reset/ResetPass";
+import { requestOTP, resetPassword } from "../../features/users/userAxios";
+import { Alert, Button } from "react-bootstrap";
 
 const ForgetPassword = () => {
+  const [showForm, setShowForm] = useState("otp");
+  const [email, setEmail] = useState("");
+  const [resp, setResp] = useState({});
+  const [timer, setTimer] = useState(90);
+  const handleOnOTPRequest = async (email) => {
+    setTimer(90);
+    setEmail(email);
+    const response = await requestOTP({
+      email,
+    });
+    setResp(response);
+    if (response.status === "success") {
+      setShowForm("reset");
+      countDown();
+    }
+  };
+
+  const countDown = () => {
+    const cd = setInterval(() => {
+      setTimer((prevTimer) => {
+        if (prevTimer <= 1) {
+          clearInterval(cd);
+          return 0;
+        }
+        return prevTimer - 1;
+      });
+    }, 1000);
+  };
+
+  const handleOnResetPassword = (obj) => {
+    resetPassword(...obj, email);
+  };
+
+  const forms = {
+    otp: <RequestOTP handleOnOTPRequest={handleOnOTPRequest} />,
+    reset: <ResetPass handleOnResetPassword={handleOnResetPassword} />,
+  };
+
   return (
-    <div>
-      <Header />
-      <Footer />
+    <div className="d-flex justify-content-center align-items-center vh-100  bg-dark">
+      <div className="bg-white p-3" style={{ width: "450px" }}>
+        {resp.message && (
+          <>
+            <Alert variant={resp.status === "success" ? "success" : "danger"}>
+              {resp.message}
+            </Alert>
+            <div className="m-3">
+              OTP not received? request otp again
+              <Button
+                variant="secondary"
+                disabled={timer > 0}
+                onClick={() => handleOnOTPRequest(email)}
+              >
+                Request OTP again in {timer}s
+              </Button>
+            </div>
+          </>
+        )}
+
+        {forms[showForm]}
+        <div className="text-end mt-3">
+          <a href="/">Login Now</a>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default ForgetPassword;
