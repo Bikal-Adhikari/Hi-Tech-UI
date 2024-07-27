4cd9efb820def96f263c934503f22a58a8bd8477
Bikal <bikaladhikari5@gmail.com>
2024-07-27 22:40:58 +1000

    Server endpoint created and axios crud operations done

---
+++ src/features/users/userAxios.js
@@ -0,0 +1,92 @@
+import { apiProcessor } from "../../services/axios";
+const userEP = import.meta.env.VITE_APP_SERVER_ROOT + "/api/v1/users";
+
+export const postNewUser = (data) => {
+  const obj = {
+    url: userEP,
+    method: "post",
+    data,
+    // isPrivate: true
+  };
+
+  return apiProcessor(obj);
+};
+
+export const verifyUserLink = (data) => {
+  const obj = {
+    url: userEP + "/user-verification",
+    method: "post",
+    data,
+  };
+
+  return apiProcessor(obj);
+};
+
+export const userLogin = (data) => {
+  const obj = {
+    url: userEP + "/login",
+    method: "post",
+    data,
+    showToast: true,
+  };
+
+  return apiProcessor(obj);
+};
+
+export const fetchUserProfile = () => {
+  const obj = {
+    url: userEP,
+    method: "get",
+    isPrivate: true,
+  };
+
+  return apiProcessor(obj);
+};
+export const editUserProfile = (userobj) => {
+  const obj = {
+    url: userEP,
+    method: "put",
+    isPrivate: true,
+    data: userobj,
+  };
+
+  return apiProcessor(obj);
+};
+export const getNewAccessJWT = () => {
+  const obj = {
+    url: userEP + "/new-accessjwt",
+    method: "get",
+    isPrivate: true,
+    isRefreshJWT: true,
+  };
+
+  return apiProcessor(obj);
+};
+
+export const logoutUser = () => {
+  const obj = {
+    url: userEP + "/logout",
+    method: "delete",
+    isPrivate: true,
+    showToast: true,
+  };
+
+  return apiProcessor(obj);
+};
+export const requestOTP = () => {
+  const obj = {
+    url: userEP + "/otp",
+    method: "post",
+  };
+
+  return apiProcessor(obj);
+};
+export const resetPassword = (data) => {
+  const obj = {
+    url: userEP + "/password/reset",
+    method: "patch",
+    data,
+  };
+
+  return apiProcessor(obj);
+};
