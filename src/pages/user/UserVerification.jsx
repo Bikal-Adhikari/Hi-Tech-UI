ad3c8c2e255bc9f2bdc2bb64f5cfe406ba9561a2
Bikal <bikaladhikari5@gmail.com>
2024-07-30 10:58:34 +1000

    User verification page created

--- src/pages/user/UserVerification.jsx
+++ src/pages/user/UserVerification.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useRef, useState } from "react";
-import { Alert, Spinner } from "react-bootstrap";
-import { useSearchParams } from "react-router-dom";
+import { Alert, Spinner, Image, Button } from "react-bootstrap";
+import { useSearchParams, Link } from "react-router-dom";
 import { verifyUserLinkAction } from "../../features/users/userAction";
+import successImg from "../../assets/images/two_factor_authentication.svg";
+import img from "../../assets/images/page_not_found.svg";
 
 const UserVerification = () => {
   const [searchParams] = useSearchParams();
@@ -12,28 +14,42 @@ const UserVerification = () => {
   const shouldCall = useRef(true);
 
   useEffect(() => {
-    //call api to verify the link
     if (shouldCall.current) {
       (async () => {
         const data = await verifyUserLinkAction({ c, e });
-
         setResp(data);
       })();
-
       shouldCall.current = false;
     }
-  }, [searchParams, c, e]);
+  }, [c, e]);
 
   return (
-    <div className="d-flex justify-content-center bg-dark align-items-center vh-100  ">
-      <div
-        className="bg-light p-3 rounded text-center"
-        style={{ width: "450px" }}
-      >
+    <div
+      className="d-flex justify-content-center align-items-center vh-100"
+      style={{
+        backgroundImage: `url(${resp.status === "success" ? successImg : img})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        color: "#fff", // Ensure text is visible on the background image
+      }}
+    >
+      <div className="p-3 rounded text-center " style={{ width: "450px" }}>
         {resp.message ? (
-          <Alert variant={resp.status === "success" ? "success" : "danger"}>
-            {resp.message}
-          </Alert>
+          <>
+            <Alert variant={resp.status === "success" ? "success" : "danger"}>
+              {resp.message}
+            </Alert>
+            {resp.status === "success" ? (
+              <Button variant="primary" as={Link} to="/signIn">
+                Proceed to Sign In
+              </Button>
+            ) : (
+              <Button variant="primary" as={Link} to="/">
+                Home page
+              </Button>
+            )}
+          </>
         ) : (
           <>
             <Spinner variant="primary" className="fs-1" />
